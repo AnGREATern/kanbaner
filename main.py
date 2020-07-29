79f54f297eaf2fc3ee1414025d3ec7107a175d5f
AnGREATern <saromanovlad@gmail.com>
2020-07-29 17:35:08 +0400

    Увеличение БД, финансы

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import sqlite3
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 from PyQt5 import uic
 user = None
 
@@ -10,11 +10,11 @@ class Enter(QWidget):
 
     def __init__(self):
         super().__init__()
-        uic.loadUi('C://Users//Максим//PycharmProjects//kanbaner1//login.ui', self)
-        self.con = sqlite3.connect('C://Users//Максим//PycharmProjects//kanbaner1//personal.db')
+        uic.loadUi('login.ui', self)
+        self.con = sqlite3.connect('personal.db')
         self.cur = self.con.cursor()
         self.pb_login.clicked.connect(self.switch)
-        memory = open('C://Users//Максим//PycharmProjects//kanbaner1//memory.txt', 'r')
+        memory = open('memory.txt', 'r')
         self.le_login.setText(memory.read())
         memory.close()
         self.new = None
@@ -36,10 +36,14 @@ class Enter(QWidget):
 class New(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('C://Users//Максим//PycharmProjects//kanbaner1//create.ui', self)
+        uic.loadUi('create.ui', self)
 
 
-        uic.loadUi('C://Users//Максим//PycharmProjects//kanbaner1//create.ui', self)
+class Finance(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('finance.ui', self)
+        #  self.table.resizeColumnsToContents()
 
 
 class Kanbaner(QMainWindow):
@@ -47,14 +51,15 @@ class Kanbaner(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        uic.loadUi('C://Users//Максим//PycharmProjects//kanbaner1//main.ui', self)
-        self.con = sqlite3.connect('C://Users//Максим//PycharmProjects//kanbaner1//personal.db')
+        uic.loadUi('main.ui', self)
+        self.con = sqlite3.connect('personal.db')
         self.cur = self.con.cursor()
         self.label.setText(user)
         self.pb_create.clicked.connect(self.create)
         self.pb_open.clicked.connect(self.open)
         self.pb_delete.clicked.connect(self.delete)
         self.pb_login.clicked.connect(self.exit)
+        self.pb_finance.clicked.connect(self.cash)
         self.title = ''
         self.rowTitles = []
         self.crew = None
@@ -65,17 +70,19 @@ class Kanbaner(QMainWindow):
         self.new.pb_complete.clicked.connect(self.vvod)
 
     def vvod(self):
-
         self.rowTitles.extend([self.new.le2.text(), self.new.le3.text(), self.new.le4.text(), self.new.le5.text(),
                               self.new.le6.text(), self.new.le7.text(), self.new.le8.text(), self.new.le9.text()])
         self.title = self.new.leName.text()
-
         self.lw.addItem(self.title)
         self.title = ''
         self.new.close()
 
     def open(self):
         pass
+
+    def cash(self):
+        self.finance = Finance()
+        self.finance.show()
 
     def delete(self):
         if [x.row() for x in self.lw.selectedIndexes()]:
